f8584d054a2c174de4473a8c660435938402846f
eros.montin <eros.montin@gmail.com>
2024-02-12 15:49:12 -0500

    v1

--- em_oai/processing.py
+++ em_oai/processing.py
@@ -3,6 +3,12 @@ import pyable_eros_montin.imaginable as ima
 
 
 
+# dess segmentation is performed voxel wise.
+# therefore we need to fix the roi files to have the same image direction, spacing and origin as the dess file
+# this function does that
+# dess_file: the dess file
+# label_file: the roi file
+# output_label_file: the output roi file
 
 def fixrois(dess_file,label_file,output_label_file):
     dess=ima.Imaginable(dess_file)
